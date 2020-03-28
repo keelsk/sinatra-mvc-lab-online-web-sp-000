da514cfcaa59969c7102aaaa08d25bbad15117a7
Kirsten Keels <keelsk@gmail.com>
2020-03-28 17:05:28 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,7 @@ class PigLatinizer < Sinatra::Base
     @text = text
   end
   
-  def starts_with_consonant
+  def starts_with_consonant(text)
     ending = text.slice!(0)
     
     while !(text[0].include?(/[aeiou]/)) do 
@@ -16,19 +16,19 @@ class PigLatinizer < Sinatra::Base
     new_text
   end
   
-  def starts_with_vowel
+  def starts_with_vowel(text)
     new_text = text + "way"
     new_text
   end
   
   def change_text
     array_piglat = []
-    array_text = text.split(" ")
+    array_text = @text.split(" ")
     array_text.each do |word|
       if (word[0].include?(/[aeiou]/))
-        array_piglat << word.starts_with_vowel
+        array_piglat << self.starts_with_vowel(word)
       else 
-        array_piglat << word.starts_with_consonant
+        array_piglat << self.starts_with_consonant(word)
       end
     end
     array_piglat.join(',')

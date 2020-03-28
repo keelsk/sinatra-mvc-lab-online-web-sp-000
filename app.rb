8cedda27d9dae52a162c56780db2c7e17c47ce6a
Kirsten Keels <keelsk@gmail.com>
2020-03-28 17:13:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post '/' do 
+  post '/piglatinize' do 
     text = PigLatinizer.new(params[:text])
     binding.pry
     @results = text.change_text

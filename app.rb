543bd47d40db0ba3951214dbb58bd036a9ed4b44
Kirsten Keels <keelsk@gmail.com>
2020-03-28 17:40:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,6 @@ class App < Sinatra::Base
   
   post '/piglatinize' do 
     @analyzed_text = PigLatinizer.new(params[:user_phrase])
-    @analyzed_text.change_text
-    binding.pry
     erb :results
   end
 end
